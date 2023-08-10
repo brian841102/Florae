6f6271cf89fdb815fcea01624c3b8b7146362e7a
brian841102 <brian841102@gmail.com>
2023-08-10 17:04:37 +0800

    update Wiki Page & location botton

--- lib/screens/wiki.dart
+++ lib/screens/wiki.dart
@@ -1,3 +1,4 @@
+import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -420,6 +421,14 @@ class _WikiChildState extends State<WikiChild> {
       else return (_offset - fullOpacityOffset) / (zeroOpacityOffset - fullOpacityOffset);
     }
   }
+  void _openGoogleMaps() async {
+    final url = Uri.parse('https://www.google.com/maps/search/?api=1&query=latitude,longitude');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not open Google Maps.';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -434,7 +443,7 @@ class _WikiChildState extends State<WikiChild> {
           physics: const BouncingScrollPhysics(),
           //physics: ClampingScrollPhysics(),
           slivers: [
-            _buildAppBar(location.imagePath,location.realName,location.birth),
+            _buildAppBar(location.imagePath, location.realName, location.birth),
             _buildCardBorder(),
             _buildGridView(),
           ],
@@ -537,6 +546,7 @@ class _WikiChildState extends State<WikiChild> {
       floating: false,
       expandedHeight: 240.0,
       stretch: true,
+      //shape: _CustomShape(), //like this,
       flexibleSpace: Stack(
         fit: StackFit.expand,
         children: [
@@ -559,11 +569,10 @@ class _WikiChildState extends State<WikiChild> {
           ),
           FlexibleSpaceBar(
             expandedTitleScale: 1,
-            stretchModes: [],
+            stretchModes: const [],
+            titlePadding: const EdgeInsets.only(left: 48, bottom: 16),
             title: Container(
-              // Wrap the title in a container to prevent zooming
               alignment: Alignment.bottomLeft, // Align the title at the bottom left
-              padding: const EdgeInsets.only(bottom: 10.0), // Adjust padding as needed
               child: SingleChildScrollView(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
@@ -571,18 +580,36 @@ class _WikiChildState extends State<WikiChild> {
                   children: <Widget>[
                     Text(realName,
                       style: const TextStyle(
-                        fontSize: 25,
+                        fontSize: 24,
                         letterSpacing: 5,
                         fontFamily: 'MPLUS',
                         fontWeight: FontWeight.normal,
                       )
                     ),
-                    Text(birth,
-                      style: const TextStyle(
-                        fontSize: 15,
-                        letterSpacing: 5,
-                        fontFamily: 'MPLUS',
-                      )
+                    Padding(
+                      padding: const EdgeInsets.only(left: 2.0),
+                      child: Row(
+                        children: [
+                          Text(birth,
+                            style: const TextStyle(
+                              fontSize: 15,
+                              letterSpacing: 5,
+                              fontFamily: 'MPLUS',
+                            )
+                          ),
+                          const SizedBox(width: 5),
+                          InkWell(
+                            onTap: () {
+                              // Open Google Maps with the provided location
+                              _openGoogleMaps();
+                            },
+                            child: const Icon(
+                              Icons.location_on,
+                              color: Colors.blue, // Customize the color of the icon
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -615,26 +642,26 @@ class _WikiChildState extends State<WikiChild> {
   }
 }
 
-Widget _buildCardBorder() {
-  return SliverWidget(
-    child: Stack(
-      children: [
-        Positioned.fill(
-          child: Container(
-            color: Colors.black,
+  Widget _buildCardBorder() {
+    return SliverWidget(
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              color: Colors.black,
+            ),
           ),
-        ),
-        Container(
-          width: double.infinity,
-          height: 50,
-          decoration: const BoxDecoration(
-            color: lightTeal,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))),
-        ),
-      ],
-    ),
-  );
-}
+          Container(
+            width: double.infinity,
+            height: 50,
+            decoration: const BoxDecoration(
+              color: lightTeal,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+          ),
+        ],
+      ),
+    );
+  }
 
 
 class SliverWidget extends StatelessWidget {
