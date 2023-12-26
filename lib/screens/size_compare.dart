3e61f7cce5ca4f199d59f8fbf9d1a45274ca4768
brian841102 <brian841102@gmail.com>
2023-12-26 17:42:09 +0800

    update SizeCompare to translate+rotate+scale

--- lib/screens/size_compare.dart
+++ lib/screens/size_compare.dart
@@ -13,7 +13,23 @@ class SizeCompare extends StatefulWidget {
 
 class _SizeCompareState extends State<SizeCompare> {
 
-  double scaleFactor = 1.0;
+  double bgScale = 2.5;
+
+  double _scale = 1.0;
+  double _lastUpdateScale = 1.0;
+
+  double _rotation = 0.0;
+  double _lastUpdateRotation = 0.0;
+
+  double _offsetX = 0.0;
+  double _pointX = 0.0;
+  double _lastUpdatePointX = 0.0;
+  double _deltaPointX = 0.0;
+
+  double _offsetY = 0.0;
+  double _pointY = 0.0;
+  double _lastUpdatePointY = 0.0;
+  double _deltaPointY = 0.0;
   @override
   void initState() {
     super.initState();
@@ -32,16 +48,51 @@ class _SizeCompareState extends State<SizeCompare> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onVerticalDragStart: (details) {
+      // onVerticalDragUpdate: (details) {
+      //   setState(() {
+      //     bgScale -= details.primaryDelta! * 0.005;
+      //     bgScale = bgScale.clamp(0.3, 20);
+      //   });
+      // },
+      onScaleStart: (details) {
+        _pointX = details.focalPoint.dx;
+        _lastUpdatePointX = details.focalPoint.dx;
+        _pointY = details.focalPoint.dy;
+        _lastUpdatePointY = details.focalPoint.dy;
+      },
+      onScaleUpdate: (details) {
+        _scale = details.scale*_lastUpdateScale;
+        _scale = _scale.clamp(0.3, 20);
+        // print("Scale: ");
+        // print(_scale);
+        _rotation = details.rotation+_lastUpdateRotation;
+        
+        _deltaPointX = (details.focalPoint.dx - _lastUpdatePointX);
+        _lastUpdatePointX = details.focalPoint.dx;
+        _deltaPointY = (details.focalPoint.dy - _lastUpdatePointY);
+        _lastUpdatePointY = details.focalPoint.dy;
+        
         setState(() {
-          //scaleFactor = 1;
-          //scaleFactor =  details.scale.clamp(0.3, 1.8);
+          _offsetX += _deltaPointX;
+          _offsetY += _deltaPointY;
         });
       },
-      onVerticalDragUpdate: (details) {
+      onScaleEnd: (details) {
+        _lastUpdateScale = _scale;
+        _lastUpdateRotation= _rotation;
+        // print("_lastUpdateScale: ");
+        // print(_lastUpdateScale);
+      },
+      onDoubleTap: () {
         setState(() {
-          scaleFactor -= details.primaryDelta! * 0.005;
-          scaleFactor = scaleFactor.clamp(0.3, 20);
+          _scale = 1.0;
+          _lastUpdateScale = 1.0;
+          _rotation = 0.0;
+          _lastUpdateRotation = 0.0;
+          _offsetX = 0.0;
+          _lastUpdatePointX = 0.0;
+          _offsetY = 0.0;
+          _lastUpdatePointY = 0.0;
         });
       },
       child: Scaffold(
@@ -53,12 +104,29 @@ class _SizeCompareState extends State<SizeCompare> {
             style: const TextStyle(color: Colors.black),
             child: Align(
               alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: 200 * scaleFactor,
-                child: FittedBox(
-                  fit: BoxFit.fitHeight,
-                  child: Image.asset('assets/images/cii_r.png'),
-                ),
+              child: Stack(
+                children: [
+                  Transform(
+                    transform: Matrix4.translationValues(_offsetX, _offsetY, 0)
+                      ..rotateZ(_rotation),
+                    child: SizedBox(
+                      height: 500 * _scale,
+                      width: double.maxFinite,
+                      child: FittedBox(
+                        fit: BoxFit.fitHeight,
+                        child: Image.asset('assets/images/cii_r.png'),
+                      ),
+                    ),
+                  ),
+                  // SizedBox(
+                  //   height: 200 * bgScale,
+                  //   width: double.maxFinite,
+                  //   child: FittedBox(
+                  //     fit: BoxFit.fitHeight,
+                  //     child: Image.asset('assets/images/cii_r.png'),
+                  //   ),
+                  // ),
+                ],
               ),
             ),
           ),
